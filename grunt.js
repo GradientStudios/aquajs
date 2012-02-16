8547471487520ad443b0d1851454413dd146fd12
Michael "Z" Goddard <mzgoddard@gmail.com>
2012-02-16 17:02:51 -0500

    grunt - get submodules

--- grunt.js
+++ grunt.js
@@ -1,4 +1,9 @@
+var spawn = require('child_process').spawn;
+
 config.init({
+  deps: {
+    'git.submodule': true
+  },
   concat: {
     'dist/aqua.js': ['src/base.js', 'src/object.js']
   },
@@ -13,5 +18,16 @@ config.init({
   }
 });
 
+task.registerBasicTask('deps', 'download dependencies', function(data, name) {
+  var done = this.async();
+
+  spawn('git', ['submodule', 'init']).on('exit', function() {
+    spawn('git', ['submodule', 'update']).on('exit', function() {
+      log.writeln('git submodule updated.');
+      done();
+    });
+  });
+});
+
 // Default task.
-task.registerTask('default', 'lint qunit concat min');
+task.registerTask('default', 'deps lint qunit concat min');
